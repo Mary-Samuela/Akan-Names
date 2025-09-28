26ff91cccdb4a58bf098de235858e17faf1acc07
Mary-Samuela <samuelamary@gmail.com>
2025-09-28 21:29:33 +0300

    do the set up of the javascript

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // Set up  
+const malenames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const femalenames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+const days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const daysofweek=[0,1,2,3,4,5,6]; 
